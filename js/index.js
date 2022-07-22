a1fc3aa429ec352e623e4e2b12481f0dd22944a2
Fidan Najaf <73224528+fidannjf@users.noreply.github.com>
2022-07-23 01:16:32 +0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// var main = document.querySelector('store-link');
+// var button = document.querySelector('add-bag-icon');
+// for( but of add-bag-icon)
+// {
+//     but.addEventListener('click',(e) =>
+//     {
+//         var add = Number(main.getAttribute('data-count') || 0 );
+//         main.setAttribute('data-count',add + 1);
+//         main.classList.add('zero');
+
+//     })
+// }
